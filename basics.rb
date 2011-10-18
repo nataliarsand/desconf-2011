9909840758271421158b62b7c5d31087eaf1b4f3
Rafael Bandeira <rafaelbandeira3@gmail.com>
2011-10-17 23:58:36 -0200

    ruby-1.9.2 notation

--- basics.rb
+++ basics.rb
@@ -12,13 +12,13 @@ class MyApp < Sinatra::Base
 
   get '/' do
     emails = {
-      'barbara' => "barbara@quavio.com.br",
-      'daniel'  => "dwildt@gmail.com",
-      'felipe'  => "felipe.benites@gmail.com",
-      'natalia' => "nataliarsand@gmail.com",
-      'pedro'   => "pedrobelleza@gmail.com",
-      'rafael'  => "rafaelbandeira3@gmail.com",
-      'mauricio' => "m@bitbonsai.com"
+      barbara: "barbara@quavio.com.br",
+      daniel: "dwildt@gmail.com",
+      felipe: "felipe.benites@gmail.com",
+      natalia: "nataliarsand@gmail.com",
+      pedro: "pedrobelleza@gmail.com",
+      rafael: "rafaelbandeira3@gmail.com",
+      mauricio: "m@bitbonsai.com"
     }.each_pair { |k, v| v.replace(Digest::MD5.hexdigest(v)) }
 
     gravatar = lambda { |name| "http://www.gravatar.com/avatar/#{emails[name]}" }
@@ -27,37 +27,37 @@ class MyApp < Sinatra::Base
       {
         href: "http://twitter.com/barbaracones",
         name: "Barbara Wolff Dick",
-        gravatar: gravatar['barbara']
+        gravatar: gravatar[:barbara]
       },
       {
         href: "http://twitter.com/dwildt",
         name: "Daniel Wildt",
-        gravatar: gravatar['daniel']
+        gravatar: gravatar[:daniel]
       },
       {
         href: "http://twitter.com/felipebcabral",
         name: "Felipe Cabral",
-        gravatar: gravatar['felipe']
+        gravatar: gravatar[:felipe]
       },
       {
         href: "http://twitter.com/nataliarsand",
         name: "Natalia Arsand",
-        gravatar: gravatar['natalia']
+        gravatar: gravatar[:natalia]
       },
       {
         href: "http://twitter.com/bitbonsai",
         name: "Mauricio Wolff",
-        gravatar: gravatar['mauricio']
+        gravatar: gravatar[:mauricio]
       },
       {
         href: "http://twitter.com/pedrobelleza",
         name: "Pedro Belleza",
-        gravatar: gravatar['pedro']
+        gravatar: gravatar[:pedro]
       },
       {
         href: "http://twitter.com/rafb3",
         name: "Rafael Bandeira",
-        gravatar: gravatar['rafael']
+        gravatar: gravatar[:rafael]
       }
     ]
     slim :index, :locals => { :people => people }
