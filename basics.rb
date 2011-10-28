fffac069fe86ba40256c3e5068f2919747a5a85d
Rafael Bandeira <rafaelbandeira3@gmail.com>
2011-10-28 00:21:30 -0200

    Registrations are open. Yay

--- basics.rb
+++ basics.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class MyApp < Sinatra::Base
   set :public, File.dirname(__FILE__) + '/public'
   set :views, File.dirname(__FILE__) + '/templates'
@@ -10,7 +12,43 @@ class MyApp < Sinatra::Base
     request.host.match(/heroku/)
   end
 
+  def registration_open?
+    true
+  end
+
+  def speaker_code
+    variants = %w{9BE5FF902D2D815DD41A6FB3E6D6797F C1B8B60BDBDB37A994A45FA1834F796A 5BB518FA8080A6F774912FBD6E460451 4B56643D1B1B54F2241C6FB96D869205}
+    variants[rand(variants.size)]
+  end
+
+  def render_index(locals = {})
+    slim :index, :locals => locals
+  end
+
   get '/' do
+    render_index
+  end
+
+  get '/paguei-seu-lindo' do
+    render_index(thankyou: 'Uhuuul! Nos vemos lá! Enquanto isso vou ficar processando seu pagamento aqui! Valeuzão!')
+  end
+
+  get '/css/:name.css' do
+    content_type 'text/css', :charset => 'utf-8'
+    sass :"sass/#{params['name']}", Compass.sass_engine_options
+  end
+
+  post '/' do
+    s = Subscriber.create({
+      :name => params[:name],
+      :email => params[:mail],
+      :created_at => Time.now
+    })
+    
+    redirect "/"
+  end
+
+  def happy_people!
     emails = {
       barbara: "barbara@quavio.com.br",
       daniel: "dwildt@gmail.com",
@@ -60,21 +98,5 @@ class MyApp < Sinatra::Base
         gravatar: gravatar[:rafael]
       }
     ]
-    slim :index, :locals => { :people => people }
-  end
-
-  get '/css/:name.css' do
-    content_type 'text/css', :charset => 'utf-8'
-    sass :"sass/#{params['name']}", Compass.sass_engine_options
-  end
-
-  post '/' do
-    s = Subscriber.create({
-      :name => params[:name],
-      :email => params[:mail],
-      :created_at => Time.now
-    })
-    
-    redirect "/"
   end
 end
