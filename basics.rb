474fbfcccf74ceb5b29e7562d600062d8a47e1ba
Rafael Bandeira <contato+rafb3@desconf.com.br>
2011-11-05 10:27:35 -0200

    registration helpers have their own place

--- basics.rb
+++ basics.rb
@@ -1,10 +1,13 @@
 # encoding: utf-8
 
 require './lib/happy_people'
+require './lib/registration'
 require './models/subscriber'
 
 class MyApp < Sinatra::Base
   include Desconf::HappyPeople
+  include Desconf::Registration
+
   set :public, File.dirname(__FILE__) + '/public'
   set :views, File.dirname(__FILE__) + '/templates'
   set :slim, pretty: true
@@ -15,15 +18,6 @@ class MyApp < Sinatra::Base
 
   def heroku_host?
     request.host.match(/heroku/)
-  end
-
-  def registration_open?
-    true
-  end
-
-  def speaker_code
-    variants = %w{9BE5FF902D2D815DD41A6FB3E6D6797F C1B8B60BDBDB37A994A45FA1834F796A 5BB518FA8080A6F774912FBD6E460451 4B56643D1B1B54F2241C6FB96D869205}
-    variants[rand(variants.size)]
   end
 
   def render_index(locals = {})
